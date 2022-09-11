dc90aa2967df62e29e987cebde7f52adffc7e9e8
elizabeth <bettychukwu291.com>
2022-09-11 14:55:29 -0700

    print std output with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+int main(void)
+	/**
+	 * code that prints stdoutput using puts
+	 *
+	 */
+{
+	puts("Programming is like building a multilingual puzzle");
+		return(0);
+}
