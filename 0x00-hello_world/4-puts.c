503bb7c55afeae2605ec02d47b2194fab29afbfd
elizabeth <bettychukwu291.com>
2022-09-11 15:25:31 -0700

    correction for puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
 
 
 /**
- * print standard output with puts
- * Return:0
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingua puzzle"
+ * Return: 0
  */
 
 int main(void)
