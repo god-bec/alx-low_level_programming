a061f39c5779636a8b9d5209342abce06bdda9fe
elizabeth <bettychukwu291.com>
2022-09-12 03:48:35 -0700

    first sizeof commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - print the string in the printf function
+ *
+ * Description: using the main function
+ * this program prints "Write a C program that prints the size of various types on the computer it is compiled and run on"
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	char = i
+	int = c
+	float = d
+
+	printf("print size of char:%c\n",(unsigned long)sizeof(i));
+	printf("print size of int:%d\n",(unsigned long)sizeof(c));
+	printf("print size of float:%d\n",(unsigned long)sizeof(d));
+return (0);
+
+}
