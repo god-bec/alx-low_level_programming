a072ec06fb958299b703be20b0e9bfb4281bbf75
elizabeth <bettychukwu291.com>
2022-09-13 08:34:08 -0700

    whitespace removal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,6 @@
  * Return:0
  */
 int main(void)
-
 {
 	int n;
 	int l;
@@ -20,13 +19,13 @@ int main(void)
 	l = n % 10;
 	if (l > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);			
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);			
 	} else if (l == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, l); 
+	printf("Last digit of %d is %d and is 0\n", n, l); 
 	} else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l); 
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l); 
 	}
 	return (0);
 
