5346d0880e8838926a991e9b497678d198785d52
elizabeth <bettychukwu291.com>
2022-09-13 09:20:12 -0700

    remove whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
-
-/* betty style doc for function main goes there */
+/**
+ * main - print number stored in the variable n which is positive or negative.
+ * Description: using the main function
+ * Return:0
+ *
+ */
 
 int main(void)
-
 {
+	int n;
 
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-
-	 }else if (n == 0)
+	}else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	 }else
+	}else
 	{
 		printf("%d is negative\n", n);
-	 } 
+	}
 	return (0);
 }
 
