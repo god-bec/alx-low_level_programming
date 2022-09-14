23c69636598940106b74f4fabc05765bc9ceba03
elizabeth <bettychukwu291.com>
2022-09-14 02:52:09 -0700

    first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @c: any value of type int
+ * Return: 0
+ *
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	 }
+	 else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (c < 0)
+	{
+	 	_putchar('-');
+		return (-1); 
+	}
+	return (0);
+}
