504d802198fcf7a882c9b1178e7000dfd01857ea
elizabeth <bettychukwu291.com>
2022-09-14 01:43:54 -0700

    first commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks for lowercase character
+ * @c: any value of type int
+ * Return: 0
+ *
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= '2'));
+}
