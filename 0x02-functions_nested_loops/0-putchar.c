04be9a48b04bc44fb12ac23b105514d0607ca8a1
elizabeth <bettychukwu291.com>
2022-09-14 13:28:21 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - write a program tht prints putchar
- * Description: using main function
+ * main - prints _putchar followed by a new line
  * Return: 0
- *
  */
 int main(void)
 {
-	char *sh = "Holberton";
-	while (*sh)
-	{
-	_putchar(*sh);
-	sh++;
-	}
- 	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
