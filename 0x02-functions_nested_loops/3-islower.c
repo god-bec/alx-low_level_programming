863176e0bd9d0e8cd9dbb86a328a439b55dad239
elizabeth <bettychukwu291.com>
2022-09-13 23:19:46 -0700

    first lstdigit commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: any value of type int
+ * Return: 0
+ *
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
