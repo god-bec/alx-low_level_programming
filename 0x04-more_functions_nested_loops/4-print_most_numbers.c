0b2ea964a183d7d97059421adf79d78606fde5f7
elizabeth <bettychukwu291.com>
2022-09-16 08:44:22 -0700

    removed whit space

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
 * print_most_numbers - prints numbers except 2 and 4
-* 
+*
 * Return: returns nothing
 */
 
@@ -12,7 +12,7 @@ int n;
 
 for (n = 48; n < 58; n++)
 {
-if((n == 50) || (n == 52))
+if ((n == 50) || (n == 52))
 {
 continue;
 }
