65b4c57531121a51b9d16755c4d7a40e598b4db9
elizabeth <bettychukwu291.com>
2022-09-16 17:58:34 -0700

    first commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* print_line - prints a staright line
-* @n: parameter
-* Return: returns nothing
+* print_line - prints a straight line
+* @n: an integer
+* Return: void
 */
 
 void print_line(int n)
 {
+char a = '-';
+
+if (n > 0)
+{
+for (; n > 0; n--)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
