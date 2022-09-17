22332376322fc6c3ebf0257b003de40e3964772c
elizabeth <bettychukwu291.com>
2022-09-16 18:34:12 -0700

    first commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,6 +17,8 @@ for (len = 0; len < n; len++)
 {
 for (space = 0; space < len; space++)
 {
+_putchar(' ');
+}
 _putchar('\\');
 
 if (len == (n - 1))
@@ -27,5 +29,4 @@ _putchar('\n');
 }
 }
 _putchar('\n');
-}
 }
