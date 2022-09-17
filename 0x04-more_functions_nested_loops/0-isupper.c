a889133aab96ce91ffb85450f6137fde68384aab
elizabeth <bettychukwu291.com>
2022-09-16 17:43:39 -0700

    first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,10 +9,5 @@
 
 int _isupper(int c)
 {
-if ((c >= 'A') && (c <= '2'))
-{
-return (1);
-}
-
-return (0);
+return (c >= 'A' && c <= 'Z');
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_line - prints a staright line
+* @n: parameter
+* Return: returns nothing
+*/
+
+void print_line(int n)
+{
