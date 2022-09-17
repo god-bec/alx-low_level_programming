d31fcc3a4cfed868054ccb3cdbdb69e5fe93f76d
elizabeth <bettychukwu291.com>
2022-09-17 04:42:12 -0700

    first commmit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,68 +1,51 @@
 #include "main.h"
+
 /**
- *
- *  * print_times_table - prints times table for numbers from 0-14
- *  @n: An input integer value
- *  Return: Nothing
+ * print_times_table - function that prints the n times table, starting with 0.
+ * @n: input of table n
+ * Return: nothing.
  */
+
 void print_times_table(int n)
 {
-	int i, j;
+int row, col;
+int mult = 0;
 
-	if (n > 0 && n < 15)
-	{
-		for (i =0; i <= n; i++)
-		{
-			_putchar('0');
-			for (j = i; j <= n; j++)
-				putformat(i * j);
-			_putchar('\n');
-		}
-
-	}
-}
-/**
- *
- *  * putformat - formatted characters to output
- *
- *   * @n: number to format
- *
- *    * Return: nothing
- *
- *     */
-void putformat(int n)
+if (n <= 15 && n >= 0)
 {
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+for (row = 0; row <= n; row++)
+{
+for (col = 0; col <= n; col++)
+{
+mult = (row * col);
+if (mult < 10)
+{
+if (col != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
 }
-
-
-
-
-
-
-
+_putchar(mult + '0');
+}
+else if (mult < 100)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((mult / 10) + '0');
+_putchar((mult % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((mult / 100) + '0');
+_putchar(((mult % 100) / 10) + '0');
+_putchar((mult % 10) + '0');
+}
+if (col != n)
+_putchar(',');
+}
+_putchar('\n');
+}
+}
+}
